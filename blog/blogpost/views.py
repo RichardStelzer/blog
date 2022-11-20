c82081efedef84b19912bacbaaa3d8515eb7c780
RichardStelzer <40047008+RichardStelzer@users.noreply.github.com>
2022-11-20 04:07:30 +0100

    Added form interaction. Submitted comments are now stored in the database.

--- blog/blogpost/views.py
+++ blog/blogpost/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Post
 from .forms import CommentForm
@@ -7,6 +7,16 @@ from .forms import CommentForm
 def detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
 
-    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)  # Gets form data and passes it to the CommentForm class
+
+        if form.is_valid():
+            comment = form.save(commit=False)  # Create temporary comment, does not submit to the database yet
+            comment.post = post  # Add foreign key and connect comment to post by doing so
+            comment.save()  # Submit comment to the database
+
+            return redirect("post_detail", slug=slug)
+    else:
+        form = CommentForm()
 
     return render(request, "blogpost/detail.html", {"post": post, "form": form})
