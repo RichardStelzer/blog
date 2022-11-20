19b47e627203135ca7ff3dc3a268d1b8d0603751
RichardStelzer <40047008+RichardStelzer@users.noreply.github.com>
2022-11-20 03:40:07 +0100

    Added a comment form under the blog post on the details page.

--- blog/blogpost/views.py
+++ blog/blogpost/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
+from .forms import CommentForm
 
 
 def detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    return render(request, "blogpost/detail.html", {"post": post})
+
+    form = CommentForm()
+
+    return render(request, "blogpost/detail.html", {"post": post, "form": form})
