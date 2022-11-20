a6284f2d2f743dbbb2c8257e1700fb59bd5bfa92
RichardStelzer <40047008+RichardStelzer@users.noreply.github.com>
2022-11-20 03:27:26 +0100

    Added model called "Comment" and updated database with the new entries.

--- blog/blogpost/models.py
+++ blog/blogpost/models.py
@@ -11,7 +11,11 @@ class Post(models.Model):
     class Meta:
         ordering = ("-date_published",)
 
-# class Comment(models.Model):
-#    content = models.TextField()
-#    author = models.CharField()
-#    date_published = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    # Connect comment with post using the ForeignKey and when post is deleted, the comments get removed as well.
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    date_published = models.DateTimeField(auto_now_add=True)
